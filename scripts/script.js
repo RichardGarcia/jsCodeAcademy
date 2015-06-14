984b9f8f19a710e3cd7b67c7017c0398e9cae973
richardgarcia <richardgarciaph@gmail.com>
2015-06-14 23:12:36 +1200

    started to docu my js tutorial for future reference.. :)

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// What is your name.. //string.. with quotes..
+console.log("myName");
+
+//Discover the length
+console.log("myName".length);
+
+//Basic math & numbers and more
+console.log(3+4);
+
+/*
+
+4*8
+12/3
+
+
+
+*/
+
+// ...to be continued.. :)
